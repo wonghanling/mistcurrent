19495412fcac5a530f7927f82526f458f06ebcd4
wonghanling <hanlingwong95@gmail.com>
2026-02-27 18:09:12 +0800

    Delete upload-bundle directory
    更改

--- upload-bundle/lib/tokens.ts
+++
@@ -1,6 +0,0 @@
-import crypto from 'crypto'
-
-export const generateMistToken = (): string => {
-  const raw = crypto.randomBytes(32).toString('base64')
-  return raw.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/g, '')
-}
